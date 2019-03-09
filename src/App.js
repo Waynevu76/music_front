2c2f47719176c16ea66a4e08a15b852ad93ebaba
Vũ Quang <quangvu7690@gmail.com>
2019-03-10 05:29:27 +0700

    qunag

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import Home from './container/HomeScreen';
 import axios from 'axios';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Artist from "./container/ArtistScreen";
+import Gerne from "./container/GerneScreen";
+import Home from "./container/HomeScreen";
+import PlayList from "./container/PlayListScreen";
+import TopTen from "./container/TopTenScreen";
+
 
 class App extends Component {
   state={};
@@ -21,9 +28,26 @@ class App extends Component {
   };
   render() {
     return (
-      <div className="App">
-        <Home/>
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact path="/" render={(props) => {
+            return <Home {...props}/>
+          }} />
+          <Route exact path="/artist" render={(props) => {
+            return <Artist {...props}/>
+          }} />
+          <Route exact path="/gerne" render={(props) => {
+            return <Gerne {...props}/>
+          }} />
+          <Route exact path="/playlist" render={(props) => {
+            return <PlayList {...props}/>
+          }} />
+          <Route exact path="/topten" render={(props) => {
+            return <TopTen {...props}/>
+          }} />
+
+        </div>
+      </Router>
     );
   }
 }
